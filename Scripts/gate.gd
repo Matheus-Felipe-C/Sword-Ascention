7974a24f63c60922bc45a93d8f18ae0dfc07f046
Matheus Felipe <theusf40@hotmail.com>
2024-02-03 19:22:52 -0300

    Bugfix: Múltiplos portões funcionam na mesma fase e refactor da árvore de cenas

--- Scripts/gate.gd
+++ Scripts/gate.gd
@@ -7,12 +7,18 @@ func _ready():
 	print("Gate script is ready")
 
 func _on_pressure_plate_body_entered(_body):
+	if _body.name != self.name:
+		pass
+	
 	if is_visible():
 		hide()
 		set_collision_layer_value(1, false)
 		set_collision_mask_value(1, false)
 
 func _on_pressure_plate_body_exited(_body):
+	if _body.name != self.name:
+		pass
+	
 	if not simpleGate:
 		show()
 		set_collision_layer_value(1, true)
