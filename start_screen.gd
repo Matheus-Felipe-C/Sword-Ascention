3c1528c03de4c533dae7236f266395157fa7c74e
Wendel Muniz <100154124+wendel211@users.noreply.github.com>
2024-02-04 11:49:00 -0300

    readme
    Criar README

---
+++ start_screen.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	$controls/startBtn.grab_focus()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_btn_pressed():
+	get_tree().change_scene_to_file("res://Cenas/Fases/main.tscn")
+	
+	pass # Replace with function body.
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+
